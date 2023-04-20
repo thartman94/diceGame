6e14ac7486c5b6fb91b748406a6794a43665b608
Tom <94thartman@gmail.com>
2023-04-20 13:19:16 -0400

    stuff

--- main.py
+++ main.py
@@ -1,11 +1,17 @@
 from game import run_game
 
 def main(players, games = 10000):
-  results = [run_game(players) for i in range(games)]
-
-  print("Average turns: ", sum([r["turns"] for r in results]) / games)
-  print("Average rounds: ", sum([r["rounds"] for r in results]) / games)
-
+  most_win_tracker = [0 for i in range(players)]
+  for i in range(100):
+    win_tracker = [0 for i in range(players)]
+  
+    for i in range(games):
+      win_tracker[run_game(players)["winner"]] += 1
+    
+    most_winner = max( (v, i) for i, v in enumerate(win_tracker) )[1]
+    most_win_tracker[most_winner] += 1
+    
+  print(most_win_tracker)
 
 if __name__ == "__main__":
-  main(6)
+  main(10)
